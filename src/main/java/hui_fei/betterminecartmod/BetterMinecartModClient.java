24a1279ef8cc1073b731f476e20c13f572408de6
hui_fei <13494611+hui__fei@user.noreply.gitee.com>
2025-05-03 19:31:38 +0800

    原始版本提交，未修改

---
+++ src/main/java/hui_fei/betterminecartmod/BetterMinecartModClient.java
@@ -0,0 +1,10 @@
+package hui_fei.betterminecartmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterMinecartModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/hui_fei/betterminecartmod/BetterMinecartModDataGenerator.java
@@ -0,0 +1,11 @@
+package hui_fei.betterminecartmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterMinecartModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
